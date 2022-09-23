7d4b27c02feb232a057123b469d771d4b9acb943
GITjaiprakas <107695687+GITjaiprakas@users.noreply.github.com>
2022-09-23 10:46:51 +0530

    Create 1Q.c

---
+++ 1Q.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+    int main(){
+        int n;
+        printf("Enter the number of times you want to print MySirG : ");
+        scanf("%d",&n);
+        for(int i=1;i<=n;i++){
+            printf("MySirG\n");
+        }
+        return 0;      
+    }
